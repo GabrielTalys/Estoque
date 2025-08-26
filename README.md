c2b65807a347627652d906a8f7e2f125ee19fd29
GabrielTalys <seu_email_do_github@gmail.com>
2025-08-26 14:20:19 -0300

    criando app core.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 [python-decouple] (https://github.com/henriquebastos/python-decouple)
 
 [package-of-the-week-python-decouple] (https://simpleisbetterthancomplex.com/20/15/11/26/package-of-the-week-python-decouple.html)
+
+[github.com/rg3915/tutoriais-django-basic](https://github.com/rg3915/tutoriais/tree/master/django-basic)
---
+++ projeto/core/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Significa que ele só vai atualizar a data quando criar alguma coisa
+class TimeStampedModel(models.Model):
+    created = models.DateTimeFieold(
+        'Criado em',
+        auto_now_add=True,
+        auto_now =False
+    )
+    # Significa que ele vai atualizar a data toda vez que modificar algum registro
+    modified = models.DateTimeField(
+        'modificado em',
+        auto_now_add=False,
+        auto_now=True    
+    )
+
+    #significa que vou usar ele como herença de classes em outros models mais a frente
+    class Meta:
+        abstract =  True
